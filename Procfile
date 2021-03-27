8bbe6948cd8fb9f84f3270b8a6a67178503f667b
arjun <arjunpanwar28oct2001@gmail.com>
2021-03-27 15:33:29 +0530

    fdbf

--- Procfile
+++ Procfile
@@ -1,4 +1,1 @@
-web: gunicorn --workers=3 app:app --timeout 200 --log-file -
-
-
-
+web: gunicorn main:app
--- main.py
+++ main.py
@@ -1,15 +1,92 @@
-from PIL import Image, ImageDraw, ImageFont
-import pandas as pd
+from flask import Flask, request, render_template
+from flask import Response
 import os
-df = pd.read_csv('list.csv')
-font = ImageFont.truetype('LibreBaskerville-Regular.ttf',100)
-for index,j in df.iterrows():
-    img = Image.open('certificate.jpg')
-    draw = ImageDraw.Draw(img)
-    image_width = img.width
-    image_height = img.height
-    text_width, _ = draw.textsize(j['name'], font=font)
+from flask_cors import CORS, cross_origin
+from prediction_Validation_Insertion import pred_validation
+from trainingModel import trainModel
+from training_Validation_Insertion import train_validation
+#import flask_monitoringdashboard as dashboard
+from predictFromModel import prediction
+
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
+
+app = Flask(__name__)
+#dashboard.bind(app)
+CORS(app)
+
+@app.route("/", methods=['GET'])
+@cross_origin()
+def home():
+    return render_template('index.html')
+
+@app.route("/predict", methods=['POST'])
+@cross_origin()
+def predictRouteClient():
+    try:
+        if request.json is not None:
+            path = request.json['filepath']
+
+            pred_val = pred_validation(path) #object initialization
+
+            pred_val.prediction_validation() #calling the prediction_validation function
+
+            pred = prediction(path) #object initialization
+
+            # predicting for dataset present in database
+            path = pred.predictionFromModel()
+            return Response("Prediction File created at %s!!!" % path)
+        elif request.form is not None:
+            path = request.form['filepath']
+
+            pred_val = pred_validation(path) #object initialization
+
+            pred_val.prediction_validation() #calling the prediction_validation function
+
+            pred = prediction(path) #object initialization
+
+            # predicting for dataset present in database
+            path = pred.predictionFromModel()
+            return Response("Prediction File created at %s!!!" % path)
+
+    except ValueError:
+        return Response("Error Occurred! %s" %ValueError)
+    except KeyError:
+        return Response("Error Occurred! %s" %KeyError)
+    except Exception as e:
+        return Response("Error Occurred! %s" %e)
 
 
-    draw.text(xy=((image_width - text_width) / 2,560),text='{}'.format(j['name']),fill=(0,0,0),font=font)
-    img.save('pictures/{}.jpg'.format(j['name']))
+
+@app.route("/train", methods=['POST'])
+@cross_origin()
+def trainRouteClient():
+
+    try:
+        if request.json['filepath'] is not None:
+            path = request.json['filepath']
+            train_valObj = train_validation(path) #object initialization
+
+            train_valObj.train_validation()#calling the training_validation function
+
+
+            trainModelObj = trainModel() #object initialization
+            trainModelObj.trainingModel() #training the model for the files in the table
+
+
+    except ValueError:
+
+        return Response("Error Occurred! %s" % ValueError)
+
+    except KeyError:
+
+        return Response("Error Occurred! %s" % KeyError)
+
+    except Exception as e:
+
+        return Response("Error Occurred! %s" % e)
+    return Response("Training successfull!!")
+
+port = int(os.getenv("PORT",5001))
+if __name__ == "__main__":
+    app.run(port=port,debug=True)
